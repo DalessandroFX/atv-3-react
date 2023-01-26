b7f28a08cffc13bc0c08019fcef54605442cb005
DalessandroFX <114603690+DalessandroFX@users.noreply.github.com>
2023-01-26 13:32:35 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Frase from './components/Frase';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dalessandro Ferreira Xavier</h1>
+      <Frase/>
+    </div>
+  );
+}
+
+export default App;
